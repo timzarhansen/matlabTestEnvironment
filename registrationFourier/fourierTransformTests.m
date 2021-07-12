88444a4f725e9b7b13c67e5f3319d513225677f9
tim-linux <timhansen93@googlemail.com>
2021-07-12 09:16:24 +0200

    commit of readinoutput slam

--- registrationFourier/fourierTransformTests.m
+++ registrationFourier/fourierTransformTests.m
@@ -6,27 +6,29 @@ pointCloud = pcread("after_voxel_second.pcd");
 
 %% create voxel grid of pcl %%
 fromTo = 30;
-numberOfPoints=240;%120;
-voxelData1 = zeros(numberOfPoints,numberOfPoints);
+numberOfPoints=64;%120;
+voxelData1 = zeros(numberOfPoints,numberOfPoints,numberOfPoints);
 
 for j=1:pointCloud.Count
     xPos=pointCloud.Location(j,1);
     yPos=pointCloud.Location(j,2);
     xIndex = cast((xPos + fromTo)/(fromTo*2) *numberOfPoints,'int16');
     yIndex = cast((yPos + fromTo)/(fromTo*2) *numberOfPoints,'int16');
-    voxelData1(xIndex,yIndex) = 1;
+    zIndex = cast((0 + fromTo)/(fromTo*2) *numberOfPoints,'int16');
+    voxelData1(xIndex,yIndex,zIndex) = 1;
 end
 
 [spectrum1,magnitude1,phase1] = plotffts(voxelData1,1);
 %% create voxel grid of pcl and shift by value%%
-voxelData2 = zeros(numberOfPoints,numberOfPoints);
+voxelData2 = zeros(numberOfPoints,numberOfPoints,numberOfPoints);
 shiftfirst = [-10,-5];
 for j=1:pointCloud.Count
     xPos=pointCloud.Location(j,1)+shiftfirst(1);
     yPos=pointCloud.Location(j,2)+shiftfirst(2);
     xIndex = cast((xPos + fromTo)/(fromTo*2) *numberOfPoints,'int16');
     yIndex = cast((yPos + fromTo)/(fromTo*2) *numberOfPoints,'int16');
-    voxelData2(xIndex,yIndex) = 1;
+    zIndex = cast((0 + fromTo)/(fromTo*2) *numberOfPoints,'int16');
+    voxelData2(xIndex,yIndex,zIndex) = 1;
 end
 
 % add two copies of this point Cloud
@@ -40,7 +42,7 @@ for j=1:pointCloud.Count
 end
 
 
-voxelData2=voxelData2 + 0.1*randn(size(voxelData2));
+voxelData2=voxelData2 + 0.001*randn(size(voxelData2));
 [spectrum2,magnitude2,phase2] =plotffts(voxelData2,2);
 
 
